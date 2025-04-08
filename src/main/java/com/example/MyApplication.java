4920d9d8f6ac83200c3b5eb151b104efeb369dca
madara1319 <mateusz.bik@op.pl>
2025-04-08 22:12:44 +0200

    reading bout java annotations and gradle

--- src/main/java/com/example/MyApplication.java
+++ src/main/java/com/example/MyApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @SpringBootApplication
 
 public class MyApplication {
+    private final int array[] = { 2, 52, 12, 51, 33, 1, 51, 3 };
+
     @RequestMapping("/number")
     Number count(Number args) {
 
@@ -17,8 +21,16 @@ public class MyApplication {
 
     @RequestMapping("/")
     String home() {
+        System.out.println("Home endpoint called!");
         return "Hello World!";
     }
+
+    @GetMapping("/array")
+    int[] test() {
+        return array;
+    }
+
+    @PostMapping()
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
